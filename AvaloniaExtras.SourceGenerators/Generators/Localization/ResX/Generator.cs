346e24f421c9d73085f96348173264d40c94193b
Aruden <84431560+ArudenKun@users.noreply.github.com>
2024-11-14 07:09:58 +0800

    Fix namespace

--- AvaloniaExtras.SourceGenerators/Generators/Localization/ResX/Generator.cs
+++ AvaloniaExtras.SourceGenerators/Generators/Localization/ResX/Generator.cs
@@ -38,7 +38,7 @@ public sealed class Generator : SourceGeneratorForTypeWithAttribute<ResXLocalize
 
         var builder = CodeBuilder.Create(symbol);
 
-        builder.SetBaseClass("AvaloniaExtras.Localization.Abstractions.BaseLocalizer");
+        builder.SetBaseClass("AvaloniaExtras.Localization.BaseLocalizer");
 
         builder
             .AddMethod("Reload")
